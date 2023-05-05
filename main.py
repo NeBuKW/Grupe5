e111550c260966964a109fef857457abe2f8e195
NeBuKW <m.a012356@gmail.com>
2023-05-05 20:09:03 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+from tkinter import *
+
+def mygtuko_veiksmas():
+
+    print( field.get() )
+
+    lbl.config( text = "Vardas: " + field.get() )
+
+def antro_mygtuko_veiksmas():
+    lbl = Label( window, text="Naujas tekstas " )
+    lbl.pack()
+
+
+window = Tk()
+window.geometry( "300x400" )
+
+
+lbl = Label( window, text="Vardas: " )
+field = Entry( window )
+
+
+btn = Button(window, text="Pridėti studentą", command=mygtuko_veiksmas )
+btn2 = Button( window, text="Pridėti naują tekstą", command=antro_mygtuko_veiksmas)
+
+
+lbl.pack()
+field.pack()
+btn.pack()
+btn2.pack()
+
+
+window.mainloop()
