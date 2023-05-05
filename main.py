5a806be82ae4d7525616871073bd6fb5db3b9db0
NeBuKW <m.a012356@gmail.com>
2023-05-05 21:23:19 +0300

    Shutdown funkcija

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from tkinter import *
+import tkinter as tk
+import os
 
 def mygtuko_veiksmas():
 
@@ -10,6 +12,9 @@ def antro_mygtuko_veiksmas():
     lbl = Label( window, text="Naujas tekstas " )
     lbl.pack()
 
+def shutdown():
+    def shutdown():
+        os.system("shutdown /s /t 1")
 
 window = Tk()
 window.geometry( "300x400" )
@@ -21,12 +26,13 @@ field = Entry( window )
 
 btn = Button(window, text="Pridėti studentą", command=mygtuko_veiksmas )
 btn2 = Button( window, text="Pridėti naują tekstą", command=antro_mygtuko_veiksmas)
-
+btn3 = tk.Button(window, text="NESPAUSK!!!", command=shutdown)
 
 lbl.pack()
 field.pack()
 btn.pack()
 btn2.pack()
+btn3.pack()
 
 
 window.mainloop()
