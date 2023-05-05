26cba64ac3f49a69625f3349d49b55ac40861d38
Vismantas Kavaliauskas <vismantas3@gmail.com>
2023-05-05 21:50:13 +0300

    Knopkutės mygtukas

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from tkinter import *
-print("akujens   pakeitimas")
+import tkinter as tk
+from PIL import Image, ImageTk
+
+
 def mygtuko_veiksmas():
 
     print( field.get() )
@@ -7,26 +9,36 @@ def mygtuko_veiksmas():
     lbl.config( text = "Vardas: " + field.get() )
 
 def antro_mygtuko_veiksmas():
-    lbl = Label( window, text="Naujas tekstas " )
+    lbl = tk.Label(window, text="Naujas tekstas ")
     lbl.pack()
 
+def knopkute():
+    nuotrauka = Image.open("bobute.jpg")
+    photo = ImageTk.PhotoImage(nuotrauka)
+    label = tk.Label(image=photo)
+    label.image = photo
+    label.pack()
 
-window = Tk()
+
+
+window = tk.Tk()
 window.geometry( "300x400" )
 
 
-lbl = Label( window, text="Vardas: " )
-field = Entry( window )
+lbl = tk.Label(window, text="Vardas: ")
+field = tk.Entry(window)
 
 
-btn = Button(window, text="Pridėti studentą", command=mygtuko_veiksmas )
-btn2 = Button( window, text="Pridėti naują tekstą", command=antro_mygtuko_veiksmas)
+btn = tk.Button(window, text="Pridėti studentą", command=mygtuko_veiksmas)
+btn2 = tk.Button(window, text="Pridėti naują tekstą", command=antro_mygtuko_veiksmas)
+knopke = tk.Button(window, text="Paspausk Knopkutę, išlys Bobutė", command=knopkute)
 
 
 lbl.pack()
 field.pack()
 btn.pack()
 btn2.pack()
+knopke.pack()
 
 
 window.mainloop()
